2e1f5a743ad8a06625cc4177722b97b05a9fb9cf
luciano <luciano.bj23@hotmail.com>
2023-07-21 23:00:39 +0100

    move files

--- log/error_calc.py
+++ log/error_calc.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pandas as pd
 from scipy.spatial.distance import euclidean
-from fastdtw import fastdtw
 from scipy.interpolate import interp1d
 
 def ler_csv(nome_arquivo):
@@ -38,8 +37,10 @@ controllers = ['pd', 'cascade', 'paralel']
 
 for controller in controllers:
     for velocity in velocitys:
-        iris_pose = ler_csv("log/csv/" + controller + velocity + "/iris_pose.csv")
-        magni_pose = ler_csv("log/csv/" + controller + velocity + "/magni_pose.csv")
+        iris_pose = ler_csv("csv/line/" + controller + velocity + "/iris_pose.csv")
+        magni_pose = ler_csv("csv/line/" + controller + velocity + "/magni_pose.csv")
+        # iris_pose = ler_csv("csv/sqr/cascate/iris_pose.csv")
+        # magni_pose = ler_csv("csv/sqr/cascate/magni_pose.csv")
 
         x = iris_pose["X Position"].to_numpy()
         y = iris_pose["Y Position"].to_numpy()
@@ -61,9 +62,40 @@ for controller in controllers:
         erro_maximo_y = max(abs(trajetoria_gostaria_interpolada[i][1] - iris_trajetoria[i][1]) for i in range(num_pontos))
 
         print(controller + " " + velocity +
-              " - EQM: " + str(eqm) + 
-              " - EAM: " + str(eam) + 
-              " - erro_maximo_x: " + str(erro_maximo_x) + 
-              " - erro_maximo_y: " + str(erro_maximo_y))
+            " - EQM: " + str(eqm) + 
+            " - EAM: " + str(eam) + 
+            " - erro_maximo_x: " + str(erro_maximo_x) + 
+            " - erro_maximo_y: " + str(erro_maximo_y))
     print("")
 
+
+for controller in controllers:
+    # iris_pose = ler_csv("csv/line/" + controller + velocity + "/iris_pose.csv")
+    # magni_pose = ler_csv("csv/line/" + controller + velocity + "/magni_pose.csv")
+    iris_pose = ler_csv("csv/sqr/cascate/iris_pose.csv")
+    magni_pose = ler_csv("csv/sqr/cascate/magni_pose.csv")
+
+    x = iris_pose["X Position"].to_numpy()
+    y = iris_pose["Y Position"].to_numpy()
+    iris_trajetoria = np.array([x, y]).T
+
+    x = magni_pose["X Position"].to_numpy()
+    y = magni_pose["Y Position"].to_numpy()
+    magni_trajetoria = np.array([x, y]).T
+
+    num_pontos = len(iris_trajetoria)
+    
+    interpolador = interp1d(np.linspace(0, 1, len(magni_trajetoria)), magni_trajetoria, axis=0)
+    trajetoria_gostaria_interpolada = interpolador(np.linspace(0, 1, num_pontos))
+
+    eqm = sum((distancia_entre_pontos(trajetoria_gostaria_interpolada[i], iris_trajetoria[i]) ** 2 for i in range(num_pontos))) / num_pontos
+    eam = sum(distancia_entre_pontos(trajetoria_gostaria_interpolada[i], iris_trajetoria[i]) for i in range(num_pontos)) / num_pontos
+
+    erro_maximo_x = max(abs(trajetoria_gostaria_interpolada[i][0] - iris_trajetoria[i][0]) for i in range(num_pontos))
+    erro_maximo_y = max(abs(trajetoria_gostaria_interpolada[i][1] - iris_trajetoria[i][1]) for i in range(num_pontos))
+
+    print(controller +
+        " - EQM: " + str(eqm) + 
+        " - EAM: " + str(eam) + 
+        " - erro_maximo_x: " + str(erro_maximo_x) + 
+        " - erro_maximo_y: " + str(erro_maximo_y))
